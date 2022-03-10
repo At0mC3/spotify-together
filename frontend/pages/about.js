3f539e2fbcbce4c27e91264a2d9f400086b5d329
Samantha <32003691+montgomerysamantha@users.noreply.github.com>
2022-03-09 22:46:39 -0600

    Add aspect ratio handling to make resizing images easier. Add padding to Navbar.

--- frontend/pages/about.js
+++ frontend/pages/about.js
@@ -3,16 +3,19 @@ import Link from 'next/link'
 import React, { Component } from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
 import { Carousel, Navbar, Container, Nav, NavDropdown } from 'react-bootstrap';
+import Ratio from 'react-ratio';
+import '../styles/Home.module.css'
 
 let items = ["/blackpink_dark.png", "/suicideboys.jpg", "olivia.jpg", "the-weeknd.jpg"];
 let itemList = items.map((item, index) => {
     return <Carousel.Item interval={3000} img={item} alt={index}>
-        <img
-            className="d-block w-100"
-            src={item}
-            alt={index}
-            style={{ width: 1920, height: 1080 }}
-        />
+        <Ratio ratio={ 16 / 9 }>
+            <img
+                className="d-block w-100"
+                src={item}
+                alt={index}
+            />
+        </Ratio>
         <Carousel.Caption>
             <h1>
                 Spotify Together - because music is best enjoyed together!
@@ -37,21 +40,23 @@ export default function About() {
 export class Navigation extends Component {
     render() {
         return (
-            <div>
-                <Navbar bg="dark" variant="dark">
-                    <Navbar.Brand href="/">Spotify Together</Navbar.Brand>
-                    <Navbar.Collapse id="spotify-tg-navbar">
-                        <Nav variant="dark">
-                            <Nav.Link href="/">Home</Nav.Link>
-                            <NavDropdown title="About" id="spotify-about-dropdown">
-                                <Nav.Link href="/about">About</Nav.Link>
-                                <Nav.Link href="https://github.com/montgomerysamantha/spotify-together">Source Code</Nav.Link>
-                            </NavDropdown>
-                            <Nav.Link href="/">Contact</Nav.Link>
-                        </Nav>
-                    </Navbar.Collapse>
-                </Navbar>
-            </div>
+            <Navbar expand="sm" bg="dark" variant="dark">
+            <Container>
+            <Nav bg="dark" variant="dark">
+                <Navbar.Brand href="/" style={{float: "left"}}>Spotify Together</Navbar.Brand>
+                <Navbar.Collapse id="spotify-tg-navbar" style={{float: "left"}}>
+
+                        <Nav.Link href="/">Home</Nav.Link>
+                        <NavDropdown title="About" id="spotify-about-dropdown">
+                            <Nav.Link href="/about">About</Nav.Link>
+                            <Nav.Link href="https://github.com/montgomerysamantha/spotify-together">Source Code</Nav.Link>
+                        </NavDropdown>
+                        <Nav.Link href="/">Contact</Nav.Link>
+
+                </Navbar.Collapse>
+            </Nav>
+            </Container>
+            </Navbar>
         )
     }
 }
